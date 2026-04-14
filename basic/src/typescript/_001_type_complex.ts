b91aa422ba2426be4461b937fc39e90101f52d54
hardstone <zhaoxuanmail@163.com>
2026-04-14 08:03:19 +0800

    [Feat] pick omit

--- basic/src/typescript/_001_type_complex.ts
+++ basic/src/typescript/_001_type_complex.ts
@@ -1,17 +1,22 @@
+// 联合类型
 type union_type = String | Number
+// 交叉类型
 type cross_type = { name: String; age: Number }
 
+// 接口类型
 interface Animal {
     name: String
     age: Number
 }
 
+// 接口类型可以继承以及多继承
 interface CatFamily extends Animal {
     canineTooth: Number
 }
 
+// 类类型
 class Human {
-    private name: String
+    name: String
     private age: Number
 
     constructor(name: String, age: Number) {
@@ -20,6 +25,7 @@ class Human {
     }
 }
 
+// 类类型可以继承，但是只能单继承
 class Student extends Human {
     private stuId: Number
     constructor(name: String, age: Number, stuId: Number) {
@@ -27,3 +33,9 @@ class Student extends Human {
         this.stuId = stuId
     }
 }
+
+// 和继承相反，提取指定类的部分属性
+type NamedHuman = Pick<Human, 'name'>
+
+// 和继承与提取指定属性相反，忽略指定类的部分属性
+type AgedHuman = Omit<Human, 'name'>
--- basic/src/typescript/_002_type_generics.ts
+++ basic/src/typescript/_002_type_generics.ts
@@ -1,9 +1,11 @@
 class Human<T> {
+    id: Number
     private name: String
     private age: Number
     private hobby: T
 
-    constructor(name: String, age: Number, hobby: T) {
+    constructor(id: Number, name: String, age: Number, hobby: T) {
+        this.id = id
         this.name = name
         this.age = age
         this.hobby = hobby
@@ -15,7 +17,8 @@ function display<T>(content: T) {
 }
 
 // 条件泛型
-type IsString<T> = T extends String ? true : false
+type IsString<T extends { id: Number }> = T extends String ? true : false
+const isString: IsString<Human<String>> = false
 
 const human = new Human<string>('zhangsan', 18, 'basketball')
 console.log(human as Human<string>)
