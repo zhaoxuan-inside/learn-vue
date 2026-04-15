43939047ff5df937043a2fe60e9f6ff40dd94e6b
hardstone <zhaoxuanmail@163.com>
2026-04-16 07:45:24 +0800

    [Feat] programe style

--- basic/src/components/_001_programe_style.vue
+++ basic/src/components/_001_programe_style.vue
@@ -5,24 +5,24 @@ export default {
     name: 'optional',
     data() {
         return {
-            opt_var: ref('this is a optional.')
+            opt_var: ref(0)
         }
     }
 }
 </script>
 
 <script setup lang="ts">
-let composite = ref('this is a composite')
+let composite = ref(0)
 </script>
 
 <template>
     <div>
         <h1>optional</h1>
-        <p>{{ opt_var }}</p>
+        <p @click="opt_var = opt_var + 1">{{ opt_var }}</p>
     </div>
 
     <div>
         <h1>composite</h1>
-        <p>{{ composite }}</p>
+        <p @click="composite = composite + 1">{{ composite }}</p>
     </div>
 </template>
