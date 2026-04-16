0f27759237a3ed1d1e69ae2f4c49aabde707fd9b
hardstone <zhaoxuanmail@163.com>
2026-04-16 08:12:58 +0800

    [Feat] watch

--- basic/src/App.vue
+++ basic/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import DefindVar from './components/_000_basic_struct.vue'
-import Sytle from './components/_001_programe_style.vue'
-import Composite from './components/_002_composite.vue'
+import Sytle from './components/_010_programe_style.vue'
+import Composite from './components/_020_comp.vue'
+import Watch from './components/_021_comp_watch.vue'
 </script>
 
 <template>
     <DefindVar />
     <Sytle />
     <Composite />
+    <Watch />
 </template>
---
+++ basic/src/components/_021_comp_watch.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed, reactive, ref, watch } from 'vue'
+
+const num_1 = ref(0)
+const num_2 = ref(0)
+const sum = computed(() => {
+    return Number(num_1.value) + Number(num_2.value)
+})
+
+watch([sum], (o, n) => {
+    console.log('old: ' + o)
+    console.log('new: ' + n)
+})
+</script>
+
+<template>
+    <div>
+        <div>num1: <input v-model="num_1" /></div>
+        <div>num2: <input v-model="num_2" /></div>
+        <div>sum: {{ sum }}</div>
+    </div>
+</template>
+
+<style></style>
