a7ea54504e980d4076573d56301f1a6286a0b35d
hardstone <zhaoxuanmail@163.com>
2026-04-17 08:06:27 +0800

    [Feat] props emits

--- basic/src/App.vue
+++ basic/src/App.vue
@@ -4,16 +4,26 @@ import Sytle from './components/_010_programe_style.vue'
 import Composite from './components/_020_comp.vue'
 import Watch from './components/_021_comp_watch.vue'
 import Lifecycle from './components/_030_lifecycle.vue'
+import Component from './components/_040_component.vue'
 
 import { ref } from 'vue'
 const mount = ref(true)
+
+const name = ref('zhangsan')
+const age = ref(18)
+
+function showUpdate(content: Number | String | undefined) {
+    console.log('content: ' + content)
+}
 </script>
 
 <template>
+    <Component :name="name" :age="age" @update="showUpdate" />
     <DefindVar />
     <Sytle />
     <Composite />
     <Watch />
+    <!-- v-if 判断变量，决定是否要加在组件 -->
     <Lifecycle v-if="mount" />
     <button @click="mount = !mount">{{ mount }}</button>
 </template>
---
+++ basic/src/components/_040_component.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>name: {{ name }}, age: {{ age }}</p>
+    <input v-model="content" @blur="handleUpdate" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{ name: String; age: Number }>()
+const emits = defineEmits<{
+    change: []
+    update: [value: Number | String | undefined]
+}>()
+
+const content = ref<Number | String>()
+function handleUpdate() {
+    emits('update', content.value)
+}
+</script>
+
+<style></style>
