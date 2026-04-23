87a9b22b7693f59f1ebaccec563877efd9f54b34
hardstone <zhaoxuanmail@163.com>
2026-04-23 22:27:38 +0800

    [Feat] state manage

--- basic/src/components/_080_state_define.ts
+++ basic/src/components/_080_state_define.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
+// State-3: 组合式编程创建并公布 defineStore 对象
 export const counterStore = defineStore('counter', () => {
     const count = ref(0);
     const increment = () => {
--- basic/src/main.ts
+++ basic/src/main.ts
@@ -2,14 +2,16 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './components/_070_index';
-// State-1: 引用pinia
+// State-1: 引用 pinia
 import { createPinia } from 'pinia';
 
 const app = createApp(App);
 // Router-5: 使用指定的路由
 app.use(router);
 
+// State-2: 创建 pinia 对象
 const pinia = createPinia();
+// State-3: 使用 pinia 对象
 app.use(pinia);
 
 app.mount('#app');
