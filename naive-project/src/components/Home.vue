d0a9d275e129af0e48de22b3a41988268e6af6b4
zhaoxuan <zhaoxuanmail@163.com>
2026-05-02 12:18:25 +0800

    [Feat] use naive ui

---
+++ naive-project/src/components/Home.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { NButton } from "naive-ui";
+</script>
+
+<template>
+  <n-space>
+    <n-button type="primary">Primary</n-button>
+    <n-button type="success">Success</n-button>
+  </n-space>
+</template>
